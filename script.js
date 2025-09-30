7f19ac93d4f21d42ccae6ae4b5bc8ef66a2b07c0
juniorh9305 <Juniorh31@nycstudents.net>
2025-09-30 09:39:27 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-let ownerName = "Your Name"; // update the part BETWEEN the "quotes"
-let userName = "YourUsername"; // same here
+let ownerName = "Junior H"; // update the part BETWEEN the "quotes"
+let userName = "juniorh9305"; // same here
 
 document.querySelectorAll(".owner-name").forEach((e) => {
     e.innerHTML = ownerName;
